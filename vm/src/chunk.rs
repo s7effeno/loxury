a7cc13ac40ad2a06fb0b070e5fbe9f7947da1128
steffeno <steffeno-etc@protonmail.com>
2024-05-02 01:51:12 +0200

    vm

--- vm/src/chunk.rs
+++ vm/src/chunk.rs
@@ -7,6 +7,10 @@ pub struct Chunk {
 }
 
 impl Chunk {
+    pub fn byte_at(&self, index: usize) -> u8 {
+        self.code[index]
+    }
+
     pub fn write(&mut self, byte: u8, coords: Coords) {
         self.code.push(byte);
         self.coords.push(coords);
@@ -15,6 +19,10 @@ impl Chunk {
     pub fn add_constant(&mut self, value: Value) -> u32 {
         self.constants.push(value);
         (self.constants.len() - 1) as u32
+    }
+
+    pub fn get_constant(&self, index: u8) -> Value {
+        self.constants[index as usize]
     }
 }
 
@@ -28,6 +36,24 @@ pub enum OpCode {
     Return,
 }
 
+impl TryFrom<u8> for OpCode {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Self::Constant),
+            1 => Ok(Self::Add),
+            2 => Ok(Self::Subtract),
+            3 => Ok(Self::Multiply),
+            4 => Ok(Self::Divide),
+            5 => Ok(Self::Negate),
+            6 => Ok(Self::Return),
+            _ => Err(())
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
 pub enum Value {
     Number(f64),
 }
--- vm/src/vm.rs
+++ vm/src/vm.rs
@@ -1,16 +1,93 @@
-use crate::chunk::{Chunk, Value};
+use crate::chunk::{Chunk, Value, OpCode};
+use std::mem::MaybeUninit;
+
+struct Stack {
+    values: [MaybeUninit<Value>; 256],
+    count: u8,
+}
+
+impl Stack {
+    fn new() -> Self {
+        Self {
+            values: unsafe {
+                MaybeUninit::uninit().assume_init()
+            },
+            count: 0,
+        }
+    }
+
+    fn push(&mut self, value: Value) {
+        assert_ne!(self.count, 255);
+        (self.values[self.count as usize]).write(value);
+        self.count += 1;
+    }
+
+    fn pop(&mut self) -> Value {
+        assert_ne!(self.count, 0);
+        self.count -= 1;
+        unsafe { (self.values[self.count as usize]).assume_init() }
+    }
+}
 
 struct Vm {
-    chunks: Chunk,
+    chunk: Chunk,
     ip: usize,
-    // stack: [Value;
+    stack: Stack,
 }
 
 impl Vm {
     fn read_byte(&mut self) -> u8 {
-        // let ret = self.chunks[self.ip];
+        let ret = self.chunk.byte_at(self.ip);
         self.ip += 1;
-        // ret
-        todo!()
+        ret
+    }
+
+    fn run(&mut self) -> Result<(), ()> {
+        loop {
+            match self.read_byte().try_into().unwrap() {
+                OpCode::Return => {
+                    println!("{:?}", self.stack.pop());
+                    return Ok(());
+                }
+                OpCode::Constant => {
+                    let idx = self.read_byte();
+                    let constant = self.chunk.get_constant(idx);
+                    self.stack.push(constant);
+                }
+                OpCode::Add => {
+                    let b = self.stack.pop();
+                    let a = self.stack.pop();
+                    match (a, b) {
+                        (Value::Number(a), Value::Number(b)) => self.stack.push(Value::Number(a + b))
+                    }
+                }
+                OpCode::Subtract => {
+                    let b = self.stack.pop();
+                    let a = self.stack.pop();
+                    match (a, b) {
+                        (Value::Number(a), Value::Number(b)) => self.stack.push(Value::Number(a - b))
+                    }
+                }
+                OpCode::Multiply => {
+                    let b = self.stack.pop();
+                    let a = self.stack.pop();
+                    match (a, b) {
+                        (Value::Number(a), Value::Number(b)) => self.stack.push(Value::Number(a * b))
+                    }
+                }
+                OpCode::Divide => {
+                    let b = self.stack.pop();
+                    let a = self.stack.pop();
+                    match (a, b) {
+                        (Value::Number(a), Value::Number(b)) => self.stack.push(Value::Number(a / b))
+                    }
+                }
+                OpCode::Negate => {
+                    match self.stack.pop() {
+                        Value::Number(n) => self.stack.push(Value::Number(-n))
+                    }
+                }
+            }
+        }
     }
 }
